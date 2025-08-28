c5a3d852207a78c943b1e4dc28d7084399f16377
Dhirajk56 <Dhirajkumarxyz13@gmail.com>
2025-08-28 20:00:34 +0530

    change in path

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Routing from "./components/Routing/routing";
-import { Navbar } from "./components/Navbar/Navbar";
+import Routing from "./components/Routing/routing.jsx";
+import { Navbar } from "./components/Navbar/Navbar.jsx";
 
 import "./App.css";
 const App = () => {
